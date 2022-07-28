d0a203c85b4356a512321d4c89b9501e16228a09
EternitySeraph <sm323@outlook.com>
2022-07-28 16:26:48 -0400

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -15,3 +15,7 @@ from sklearn.preprocessing import LabelEncoder
 fig = px.violin(heart_data, y="age", x="smoking", color="DEATH_EVENT", box=True, points="all", hover_data=heart_data.columns)
 fig.update_layout(title_text="Analysis in Age and Smoking on Survival Status")
 fig.show()
+
+fig = px.violin(heart_data, y="age", x="diabetes", color="DEATH_EVENT", box=True, points="all", hover_data=heart_data.columns)
+fig.update_layout(title_text="Analysis in Age and Diabetes on Survival Status")
+fig.show()
