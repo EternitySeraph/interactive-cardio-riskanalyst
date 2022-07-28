87b859d4814296e21f7fc5cb027def925caf5c2d
EternitySeraph <sm323@outlook.com>
2022-07-28 16:40:13 -0400

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,16 +1,32 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
 import os
-import numpy as np 
-import pandas as pd 
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+import matplotlib.pyplot as plt
 import warnings
 import seaborn as sns
-import matplotlib.pyplot as plt
+from colorama import Fore, Back, Style 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score
+from mlxtend.plotting import plot_confusion_matrix
+from plotly.offline import plot, iplot, init_notebook_mode
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 import plotly.express as px
+from statsmodels.formula.api import ols
+import plotly.graph_objs as gobj
+
+init_notebook_mode(connected=True)
 warnings.filterwarnings("ignore")
-pd.set_option("display.max_rows",None)
-from sklearn import preprocessing
-import matplotlib 
-matplotlib.style.use('ggplot')
-from sklearn.preprocessing import LabelEncoder
+import plotly.figure_factory as ff
+
+%matplotlib inline
+
+import xgboost
 
 fig = px.violin(heart_data, y="age", x="smoking", color="DEATH_EVENT", box=True, points="all", hover_data=heart_data.columns)
 fig.update_layout(title_text="Analysis in Age and Smoking on Survival Status")
@@ -25,3 +41,9 @@ Features = ['anemia','hypertension','serum_creatinine']
 x = heart_data[Features]
 y = heart_data["DEATH_EVENT"]
 x_train,x_test,y_train,y_test = train_test_split(x,y, test_size=0.2, random_state=2)
+
+
+xgb_clf = xgboost.XGBRFClassifier(max_depth=3, random_state=1)
+xgb_clf.fit(x_train,y_train)
+xgb_pred = xgb_clf.predict(x_test)
+xgb_acc = accuracy_score(y_test, xgb_pred)
