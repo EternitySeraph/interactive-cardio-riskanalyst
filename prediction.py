4ffff45a127695763c6bd6e7cb369de01f0be804
EternitySeraph <sm323@outlook.com>
2022-07-28 16:15:59 -0400

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -12,3 +12,6 @@ import matplotlib
 matplotlib.style.use('ggplot')
 from sklearn.preprocessing import LabelEncoder
 
+fig = px.violin(heart_data, y="age", x="smoking", color="DEATH_EVENT", box=True, points="all", hover_data=heart_data.columns)
+fig.update_layout(title_text="Analysis in Age and Smoking on Survival Status")
+fig.show()
