ebb6ef2a25e28759d62525c0b70f710a333e48aa
EternitySeraph <sm323@outlook.com>
2022-08-05 11:32:24 -0400

    Update __init__.py

--- src/flaskr/__init__.py
+++ src/flaskr/__init__.py
@@ -8,6 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
+        # path where the SQLite database file will be saved. It’s under app.instance_path, which is the path that Flask has chosen for the instance folder. 
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
