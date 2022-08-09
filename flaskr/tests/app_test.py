33e98642d33e20710c1af40b43452adadf047b3e
EternitySeraph <sm323@outlook.com>
2022-08-09 14:08:10 -0400

    Update app_test.py

--- flaskr/tests/app_test.py
+++ flaskr/tests/app_test.py
@@ -4,3 +4,7 @@ from project.app import app
 def test_index():
     tester = app.test_client()
     response = tester.get("/", content_type="html/text")
+    
+    
+def test_database():
+    assert Path("flaskr.db").is_file()
