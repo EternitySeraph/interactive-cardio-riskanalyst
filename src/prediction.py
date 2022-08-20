4748fa18631d9df4a77c394a651ea997617aea98
EternitySeraph <sm323@outlook.com>
2022-08-20 13:35:26 -0400

    some dash.plotly html prep

--- src/prediction.py
+++ src/prediction.py
@@ -28,24 +28,93 @@ import plotly.figure_factory as ff
 
 from sklearn.linear_model import LogisticRegression
 
+# all records in the csv file
 heart_data = pd.read_csv('/kaggle/input/heart-failure-clinical-data/heart_failure_clinical_records_dataset.csv')
 
+# reusable table gen component (first 10 rows)
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+# assigning webpage attribute
+app = Dash(__name__)
+
+# graph depicts data points as age and smoking as chance of death event
 fig = px.violin(heart_data, y="age", x="smoking", color="DEATH_EVENT", box=True, points="all", hover_data=heart_data.columns)
 fig.update_layout(title_text="Analysis in Age and Smoking on Survival Status")
 fig.show()
 
+# graph depicts data points as age and diabetes as chance of death event
 fig = px.violin(heart_data, y="age", x="diabetes", color="DEATH_EVENT", box=True, points="all", hover_data=heart_data.columns)
 fig.update_layout(title_text="Analysis in Age and Diabetes on Survival Status")
 fig.show()
 
+# generates table of first ten rows from csv data points
+app.layout = html.Div([
+    html.H4(children='Patient Data of Cardiac Patients'),
+    generate_table(df)
+])
 
+# age text input
+html.Label('Age:'),
+        dcc.Input(value=' ', type='number'),
+
+# sex radio selection
+html.Label('Sex:'),
+        dcc.RadioItems(['Male', 'Female'], 'Female'),
+    ], style={'padding': 10, 'flex': 1}),
+    
+# aneamia radio selection
+html.Label('Aneamia:'),
+        dcc.RadioItems(['Yes', 'No'], 'No'),
+    ], style={'padding': 10, 'flex': 1}),
+
+# high blood pressure radio selection
+html.Label('Hypertension:'),
+        dcc.RadioItems(['Yes', 'No'], 'No'),
+    ], style={'padding': 10, 'flex': 1}),
+
+# creatine phosphokinase text input or slider?
+html.Label('Creatine Phosphokinase (mcg/L):')
+dcc.Slider(
+            min=0,
+            max=1200,
+            marks={i: f'{i}' if i % 10 == 0 && i < 500 else if i % 100 == 0 for i in range(10, 1200)},
+            value=5,
+        )
+
+# platelets text input or slider?
+html.Label('Platelet Count:')
+dcc.Slider(
+            min=20000,
+            max=850000,
+            marks={i: f'{i}' if i % 500 == 0 else str(i) for i in range(20000, 850000)},
+            value=5,
+        )
+# serum sodium text input or slider?
+html.Label('Serum Sodium (mEq/L):')
+dcc.Slider(
+            min=100,
+            max=200,
+            marks={i: f'{i}' if i % 10 == 0 for i in range(100, 200)},
+            value=5,
+        )
+
+# training and testing data sets from targeted features to predict death event
 Features = [ 'anaemia', 'high_blood_pressure', 'age']
 x = heart_data[Features]
 y = heart_data["DEATH_EVENT"]
 x_train,x_test,y_train,y_test = train_test_split(x,y, test_size=0.2, random_state=2)
 
-# logistic regression
-
+# logistic regression fit, train, tested
 log_reg = LogisticRegression()
 log_reg.fit(x_train, y_train)
 log_reg_pred = log_reg.predict(x_test)
